027c329bcac7cbb843420d2e24ca83dfd29b2b1c
Byron Wheeler <bwheeler@dmp.com>
2016-06-20 14:11:17 -0500

    fixed error

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 var App = angular.module('App', [])
-App.angular.module('demo', []).controller('DemoCtrl', function($scope, $http) {
+App.controller('DemoCtrl', function($scope, $http) {
     $http.get('data.json')
     .then(function(res) {
         $scope.modules = res.data;
