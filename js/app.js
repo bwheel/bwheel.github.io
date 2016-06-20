244f8251fde5be05d70538223975ff332f5871f6
Byron Wheeler <bwheeler@dmp.com>
2016-06-20 14:13:16 -0500

    looking for data in wrong folder

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 var App = angular.module('App', [])
 App.controller('DemoCtrl', function($scope, $http) {
-    $http.get('data.json')
+    $http.get('js/data.json')
     .then(function(res) {
         $scope.modules = res.data;
     });
