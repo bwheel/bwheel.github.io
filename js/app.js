486b81f5279284b5e05c829bd0ce7b233beefe47
Byron Wheeler <bwheeler@dmp.com>
2016-06-21 10:23:07 -0500

    I scaffolded out the controllers and raw data for each section on the page.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,37 @@
 var App = angular.module('App', [])
-App.controller('DemoCtrl', function($scope, $http) {
-    $http.get('js/data.json')
-    .then(function(res) {
-        $scope.modules = res.data;
-    });
 
+
+App.controller('educationController', function($scope, $http){
+    $http.get('data/education.json')
+    .then(function (res) {
+        $scope.education = res.data;
+    });
+});
+
+App.controller('skillsController', function($scope, $http){
+    $http.get('data/skills.json')
+    .then(function (res) {
+        $scope.skills = res.data;
+    });
+});
+
+App.controller('repoController', function($scope, $http) {
+    $http.get('data/repositories.json')
+    .then(function(res) {
+        $scope.repositories = res.data;
+    });
+});
+
+App.controller('jobController', function($scope, $http) {
+    $http.get('data/jobs.json')
+    .then(function(res) {
+        $scope.jobs = res.data;
+    });
+});
+
+App.controller('awardController', function($scope, $http) {
+    $http.get('data/awards.json')
+    .then(function(res) {
+        $scope.award = res.data;
+    });
 });
