6d77830e1595f1e4d7e2be69128e79ab12534195
Byron Wheeler <bwheeler@dmp.com>
2016-06-23 15:43:59 -0500

    re organized the folder structure and switched to using githubs api for getting data.

---
+++ app/js/controllers/mainController.js
@@ -0,0 +1,34 @@
+(function(){
+    
+    var App = angular.module('MyApp', []);
+    
+
+    var MainController = function($scope, $http){
+       
+        // return a the error message.
+        var onError = function(reason){
+            $scope.error = reason;
+        };
+
+        // Save the repositories.
+        var onRepoComplete = function(response){
+            $scope.repos = response.data;
+        };
+
+        // Save the User information.
+        var onUserComplete = function(response){
+            $scope.user = response.data;
+            $http.get(response.data.repos_url)
+            .then(onRepoComplete, onError);
+        };
+
+        // GET the User and Repo information from GitHub.
+        $http.get("https://api.github.com/users/bwheel")
+        .then(onUserComplete, onError);
+    };
+
+    //Apply the controller to Angular JS
+    App.controller('mainController', MainController);
+
+
+}());
